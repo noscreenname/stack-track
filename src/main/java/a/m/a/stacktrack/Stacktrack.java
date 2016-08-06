7a8f21224f2e355b47ad1ab6b0a4f7a8378a09f4
andrey <a.mandyev@gmail.com>
2016-08-06 15:06:49 +0200

    init mvn project

---
+++ src/main/java/a/m/a/stacktrack/Stacktrack.java
@@ -0,0 +1,9 @@
+package a.m.a.stacktrack;
+
+public final class Stacktrack {
+    public static void main(String[] args) {
+        System.out.println("Starting stack-track");
+        System.out.println("Stack-track has stopped");
+        System.exit(0);
+    }
+}
